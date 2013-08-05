cbef353df9e125a4b8fb88b2a715a5cba2062830
vaishnav <vaishnavdesai@gmail.com>
2013-08-05 11:43:27 -0700

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Graph {
+  private ArrayList<Vertex> vertices;
+	public Graph(int numberVertices){
+		vertices = new ArrayList<Vertex>(numberVertices);
+		for(int i=0;i<numberVertices;i++){
+			vertices.add(new Vertex("v"+Integer.toString(i)));
+		}
+	}
+	
+	public void addEdge(int src, int dest, int weight){
+		Vertex s = vertices.get(src);
+		Edge new_edge = new Edge(vertices.get(dest),weight);
+		s.neighbours.add(new_edge);
+	}
+	
+	public ArrayList<Vertex> getVertices() {
+		return vertices;
+	}
+	
+	public Vertex getVertex(int vert){
+		return vertices.get(vert);
+	}
+}
